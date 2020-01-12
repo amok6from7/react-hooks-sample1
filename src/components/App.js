54ededd5e30aac080080ea381cbc0a81f3e3edda
amok6from7 <kwifi7@gmail.com>
2020-01-12 22:37:28 +0900

    add context

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import EventForm from './EventForm';
 
 import Events from './Events';
+import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
+
+console.log(AppContext)
 
 function App() {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}></EventForm>
-      <Events state={state} dispatch={dispatch}></Events>
-    </div>
+    <AppContext.Provider value={"This is Provider."}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}></EventForm>
+        <Events state={state} dispatch={dispatch}></Events>
+      </div>
+    </AppContext.Provider>
   );
 }
 
