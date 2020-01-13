cffeb70b4799ef1b1ebb7c4e3d733d49c7874ad1
amok6from7 <kwifi7@gmail.com>
2020-01-13 09:36:38 +0900

    create operation log

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,11 +1,17 @@
-import React, { useState, useContext } from "react";
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
-import AppContext from "../contexts/AppContext";
+import React, { useState, useContext } from 'react';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions';
+import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
   const addEvent = e => {
     e.preventDefault();
     dispatch({
@@ -13,51 +19,63 @@ const EventForm = () => {
       title,
       body
     });
-    setTitle("");
-    setBody("");
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+    setTitle('');
+    setBody('');
   };
 
   const deleteAllEvents = e => {
     e.preventDefault();
-    const result = window.confirm("すべてのイベントを削除してもよいですか？");
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    const result = window.confirm('すべてのイベントを削除してもよいですか？');
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      });
+    }
   };
 
-  const unCreatable = title === "" || body === "";
+  const unCreatable = title === '' || body === '';
   const unDeletable = state.events.length === 0;
   return (
     <>
       <h4>イベント作成フォーム</h4>
       <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
+        <div className='form-group'>
+          <label htmlFor='formEventTitle'>タイトル</label>
           <input
-            type="text"
-            className="form-control"
-            id="formEventTitle"
+            type='text'
+            className='form-control'
+            id='formEventTitle'
             value={title}
             onChange={e => setTitle(e.target.value)}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="formEventBody">ボディー</label>
+        <div className='form-group'>
+          <label htmlFor='formEventBody'>ボディー</label>
           <textarea
-            className="form-control"
-            id="formEventBody"
+            className='form-control'
+            id='formEventBody'
             value={body}
             onChange={e => setBody(e.target.value)}
           />
         </div>
 
         <button
-          className="btn btn-primary"
+          className='btn btn-primary'
           onClick={addEvent}
           disabled={unCreatable}
         >
           イベントを作成する
         </button>
         <button
-          className="btn btn-danger"
+          className='btn btn-danger'
           onClick={deleteAllEvents}
           disabled={unDeletable}
         >
